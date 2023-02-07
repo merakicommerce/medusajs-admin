ed5170017d00cef9f3dfdf62da5308195392b154
ngohoanglong <ngohoanglongptit@gmail.com>
2023-02-07 15:02:32 +0700

    update

--- src/domain/feeds/index.tsx
+++ src/domain/feeds/index.tsx
@@ -1,8 +1,10 @@
+import { Region } from "@medusajs/medusa"
+import getUnicodeFlagIcon from "country-flag-icons/unicode"
 import { useAdminRegions } from "medusa-react"
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import PageDescription from "../../components/atoms/page-description"
-import ChevronRightIcon from "../../components/fundamentals/icons/chevron-right-icon"
+import Button from "../../components/fundamentals/button"
 import MapPinIcon from "../../components/fundamentals/icons/map-pin-icon"
 const SettingsOverview: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
@@ -19,33 +21,25 @@ const SettingsOverview: React.FC<React.PropsWithChildren> = ({ children }) => {
 }
 
 type SettingsCardProps = {
+  countryCode: string
   icon: React.ReactNode
+  buttons: React.ReactNode
   heading: string
   description: string
-  to: string
   externalLink?: string
-  disabled: boolean
 }
 const SettingsCard: React.FC<SettingsCardProps> = ({
   icon,
+  countryCode,
   heading,
   description,
-  to = null,
-  externalLink = null,
-  disabled = false,
+  buttons = null,
 }) => {
-  if (disabled) {
-    to = null
-  }
-
   return (
-    <a href={to} target={"_blank"} className="flex items-center flex-1">
-      <button
-        className="flex items-center flex-1 h-full border group bg-grey-0 rounded-rounded p-large border-grey-20"
-        disabled={disabled}
-      >
-        <div className="flex items-center justify-center h-2xlarge w-2xlarge bg-violet-20 rounded-circle text-violet-60 group-disabled:bg-grey-10 group-disabled:text-grey-40">
-          {icon}
+    <div className="flex items-center flex-1">
+      <div className="flex items-start flex-1 h-full border group bg-grey-0 rounded-rounded p-large border-grey-20">
+        <div className="flex items-center justify-center text-3xl h-2xlarge w-2xlarge bg-violet-20 rounded-circle text-violet-60 group-disabled:bg-grey-10 group-disabled:text-grey-40">
+          {getUnicodeFlagIcon(countryCode) || icon}
         </div>
         <div className="flex-1 text-left mx-large">
           <h3 className="m-0 inter-large-semibold text-grey-90 group-disabled:text-grey-40">
@@ -54,31 +48,89 @@ const SettingsCard: React.FC<SettingsCardProps> = ({
           <p className="m-0 inter-base-regular text-grey-50 group-disabled:text-grey-40">
             {description}
           </p>
+          <div className="flex gap-3 mt-3 text-grey-40 group-disabled:text-grey-30">
+            {buttons}
+          </div>
         </div>
-        <div className="text-grey-40 group-disabled:text-grey-30">
-          <ChevronRightIcon />
-        </div>
-      </button>
-    </a>
+      </div>
+    </div>
   )
 }
+
 const Index = () => {
-  const { regions, isLoading } = useAdminRegions(undefined, {
+  const { regions } = useAdminRegions(undefined, {
     onSuccess: ({ regions, count }) => {
       console.log({ regions, count })
     },
   })
-  console.log({ regions })
+  const renderButtons = (r: Region) => {
+    const countryCode = r.countries[0].iso_2
+    let result = null
+    switch (countryCode) {
+      case "ie":
+        result = (
+          <>
+            <Button size="small" variant="secondary">
+              <a
+                target={"_blank"}
+                href={`https://medusa.designereditions.com/api/feed?secret=all&lang=en&currencyCode=${r.currency_code}&reg=${r.id}`}
+              >
+                English - {r.name}
+              </a>
+            </Button>
+          </>
+        )
+        break
+      case "gb":
+        result = (
+          <>
+            <Button size="small" variant="secondary">
+              <a
+                target={"_blank"}
+                href={`https://medusa.designereditions.com/api/feed?secret=all&reg=${r.id}`}
+              >
+                {r.name}
+              </a>
+            </Button>
+          </>
+        )
+        break
+      default:
+        result = (
+          <>
+            <Button size="small" variant="secondary">
+              <a
+                target={"_blank"}
+                href={`https://medusa.designereditions.com/api/feed?secret=all&reg=${r.id}`}
+              >
+                {r.name}
+              </a>
+            </Button>
+            <Button size="small" variant="secondary">
+              <a
+                target={"_blank"}
+                href={`https://medusa.designereditions.com/api/feed?secret=all&lang=en&currencyCode=${r.currency_code}&reg=${r.id}`}
+              >
+                English - {r.currency_code}
+              </a>
+            </Button>
+          </>
+        )
+        break
+    }
+    return result
+  }
   return (
     <SettingsOverview>
-      {regions.map((r) => {
+      {regions?.map((r) => {
         return (
           <SettingsCard
             key={r.id}
+            countryCode={r.name === "EU" ? "EU" : r.countries[0].iso_2}
             heading={`${r.name} (${r.currency_code})`}
             description={new Date().toLocaleString()}
             icon={<MapPinIcon />}
-            to={`https://medusa.designereditions.com/api/feed?secret=all&reg=${r.id}`}
+            buttons={renderButtons(r)}
           />
         )
       })}
@@ -87,7 +139,7 @@ const Index = () => {
 }
 
 const Feeds = () => (
-  <Routes className="h-full">
+  <Routes>
     <Route index element={<Index />} />
   </Routes>
 )
